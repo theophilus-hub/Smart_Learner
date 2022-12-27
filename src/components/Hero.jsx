3987fb4c0a9ada97566eda52a15ece7914b960e7
tarri9192 <tarri9192@gmail.com>
2022-12-27 18:41:27 +0100

    second commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,9 +13,9 @@ const Hero = () => {
     <div name='home' className='w-full h-screen bg-zinc-200 flex flex-col justify-between'>
         <div className='grid md:grid-cols-2 max-w-[1240px] m-auto'>
             <div className='flex flex-col justify-center md:items-start w-full px-2 py-8'>
-                <p className='text-2xl'>Unique Sequencing & Production</p>
-                <h1 className='py-3 text-5xl md:text-7xl font-bold'>Cloud Management</h1>
-                <p className='text-2xl'>This is our Tech brand.</p>
+                <p className='text-2xl'>Unique, Efficient & Productive</p>
+                <h1 className='py-3 text-5xl md:text-7xl font-bold'>Smart <br></br>Learner</h1>
+                <p className='text-2xl'>your very own AI tutor</p>
                 <button className='py-3 px-6 sm:w-[60%] my-4'>Get Started</button>
             </div>
             <div>
